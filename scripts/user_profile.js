7582787370f13ae281be22d5e0be96b4d570301a
FadyNF <fadynf05@gmail.com>
2025-03-13 19:13:58 +0200

    add user profile page and update navigation; implement basic edit functionality

---
+++ scripts/user_profile.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const editButton = document.querySelector(".btn-primary");
+    
+    editButton.addEventListener("click", function () {
+        alert("Edit Profile functionality will be implemented soon!");
+    });
+});
